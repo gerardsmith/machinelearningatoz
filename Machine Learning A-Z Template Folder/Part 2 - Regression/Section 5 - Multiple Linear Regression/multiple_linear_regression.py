3e02d689b97677eefe9780658a2ab7d191817770
gerardsmith <gerardhaydensmith@gmail.com>
2017-11-20 18:02:35 +0200

    MLR complete

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -14,7 +14,7 @@ y = dataset.iloc[:, 4].values
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder = LabelEncoder()
 X[:, 3] = labelencoder.fit_transform(X[:, 3])
-onehotencoder = OneHotEncoder(categorical_features = [3])
+onehotencoder = OneHotEncoder(categorical_features=[3])
 X = onehotencoder.fit_transform(X).toarray()
 
 # Avoiding the Dummy Variable Trap
@@ -22,7 +22,7 @@ X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 # Feature Scaling
 """from sklearn.preprocessing import StandardScaler
@@ -39,3 +39,30 @@ regressor.fit(X_train, y_train)
 
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+# building the optimal model using Bbackward Elimination
+
+import statsmodels.formula.api as sm
+X = np.append(np.ones((50, 1)).astype(int), X, 1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
+regressor_OLS = sm.OLS(y, X_opt).fit()
+print(regressor_OLS.summary())
+# remove hihest significance value above 5% (2)
+X_opt = X[:, [0, 1, 3, 4, 5]]
+regressor_OLS = sm.OLS(y, X_opt).fit()
+print(regressor_OLS.summary())
+
+# remove hihest significance value above 5% (1)
+X_opt = X[:, [0, 3, 4, 5]]
+regressor_OLS = sm.OLS(y, X_opt).fit()
+print(regressor_OLS.summary())
+
+# remove hihest significance value above 5% (4)
+X_opt = X[:, [0, 3, 5]]
+regressor_OLS = sm.OLS(y, X_opt).fit()
+print(regressor_OLS.summary())
+
+# remove hihest significance value above 5% (5)
+X_opt = X[:, [0, 3]]
+regressor_OLS = sm.OLS(y, X_opt).fit()
+print(regressor_OLS.summary())
